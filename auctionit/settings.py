9ed74d826dd566408b468c0a67b1b8d3a8b5d408
Stuart Hopkins <stuart.hopkins@aggiemail.usu.edu>
2019-10-24 15:12:22 -0600

    Fixed bugs

--- auctionit/settings.py
+++ auctionit/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'auctions.apps.AuctionsConfig',
+    'phone_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phone_field'
 ]
 
 MIDDLEWARE = [
--- auctions/models.py
+++ auctions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Auction(models.Model):
-    auction_id = models.CharFile(max_length=32)
+    auction_id = models.CharField(max_length=32)
 
     def __str__(self):
         return "%s the auction" % self.auction_id
@@ -28,11 +28,33 @@ class Item(models.Model):
     # Member Variables
     description = models.CharField(max_length=1000)
     current_price = models.DecimalField(max_digits=8, decimal_places=2)
+    start_date = models.DateTimeField('start date')
+    end_date = models.DateTimeField('end date')
     sold = models.BooleanField(default=False)
-    hidden = models.BooleanFiled(default=False)
+    hidden = models.BooleanField(default=False)
 
     def __str__(self):
         return "%s the item: (description) %s" % (self.name, self.description)
+
+
+class Person(models.Model):
+    # Dependencies
+    # Member Variables
+    name = models.CharField(max_length=128)
+    # phone_number = PhoneField(blank=True, help_text='Contact phone number')  # https://pypi.org/project/django-phone-field/
+
+    def __str__(self):
+        return "This Person's name is %s" % self.name
+
+
+class Bidder(models.Model):
+    # Dependencies
+    personal_info = models.OneToOneField(Person, on_delete=models.CASCADE)
+
+    # Member Variables
+
+    def __str__(self):
+        return "This bidder's name is %s" % (self.personal_info.name)
 
 
 class Bid(models.Model):
@@ -45,21 +67,11 @@ class Bid(models.Model):
 
     def __str__(self):
         return "Bid for the item %s at the price of %s by the user %s" \
-                % (self.item, self.price, self.bidder.personal_info.name)
+               % (self.item, self.price, self.bidder.personal_info.name)
 
 
-class Bidder(models.Model):
-    # Dependencies
-    personal_info = models.OneToOneField(Person)
-
-    # Member Variables
-
-
-class Person(models.Model):
-    # Dependencies
-    # Member Variables
-    name = models.CharField(max_length = 128)
-    phone_number = PhoneField(blank=True, help_text='Contact phone number')  # https://pypi.org/project/django-phone-field/
 
 
 # TODO: Do we need to make an admin class or does it do that for us?
+
+
