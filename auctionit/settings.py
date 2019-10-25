e1a91cec98c90da8639e60ee0dc36b9392aebc34
stuhops <stuart.hopkins@aggiemail.usu.edu>
2019-10-25 09:49:37 -0600

    Commented out phone_field

--- auctionit/settings.py
+++ auctionit/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'auctions.apps.AuctionsConfig',
-    'phone_field',
+    # 'phone_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
