3cd1278ac3108cd26cb9d48a6b820c70229f9919
Kohl  Kennedy <kohl.kennedy@aggiemail.usu.edu>
2019-10-26 23:54:44 -0600

    Removed the spring documentation, changed the readme and added some early unit tests classes

--- auctions/tests.py
+++ auctions/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from auctions.models import Auction, Category, Item, Person, Bid
 
 # Create your tests here.
+
+
+class AuctionTestCase(TestCase):
+    def setUp(self):
+        Auction.objects.create(auction_id="test_item")
+
+    def test_action_class(self):
+        test_item = Auction.objects.get(auction_id="test_item")
+        self.assertEqual(print(test_item), "test_item")
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Auction.objects.create(auction_id="test_item")
+
+    def test_action_class(self):
+        test_item = Auction.objects.get(auction_id="test_item")
+        self.assertEqual(print(test_item), "test_item")
+class ItemTestCase(TestCase):
+    def setUp(self):
+        Auction.objects.create(auction_id="test_item")
+
+    def test_action_class(self):
+        test_item = Auction.objects.get(auction_id="test_item")
+        self.assertEqual(print(test_item), "test_item")
+class PersonTestCase(TestCase):
+    def setUp(self):
+        Auction.objects.create(auction_id="test_item")
+
+    def test_action_class(self):
+        test_item = Auction.objects.get(auction_id="test_item")
+        self.assertEqual(print(test_item), "test_item")
+class BidTestCase(TestCase):
+    def setUp(self):
+        Auction.objects.create(auction_id="test_item")
+
+    def test_action_class(self):
+        test_item = Auction.objects.get(auction_id="test_item")
+        self.assertEqual(print(test_item), "test_item")
